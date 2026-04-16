2ec7439384f22c98c44439340bc8cc1de877d535
JohnPalmTree <johnnevanss526@gmail.com>
2026-04-15 23:59:00 -0400

    Session 3 - Area layout + nodes created.

---
+++ Assets/Scripts/ZombieNode.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ZombieNode : MonoBehaviour
+{
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
